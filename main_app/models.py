8a4096a0c1415a404ac0fc453d4cc6ffc564b08f
nisha09 <nisha1947@gmail.com>
2022-05-10 20:17:02 +0530

    add create functionality from model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    genre = models.CharField(max_length=100)
+    read = models.CharField(max_length=100)
+
+def __str__(self):
+        return self.title
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
 
-# Create your views here.
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def books_index(request):
+  books = Book.objects.all()
+  return render(request, 'books/index.html', { 'books': books })
