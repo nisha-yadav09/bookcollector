86972a1214100b6796097c90c51d61e058fa86c2
nisha09 <nisha1947@gmail.com>
2022-05-13 18:01:05 +0530

    lab 4

--- main_app/views.py
+++ main_app/views.py
@@ -22,12 +22,12 @@ def books_index(request):
 
 def books_detail(request, book_id):
   book = Book.objects.get(id=book_id)
-
   reader_form = ReaderForm()
   return render(request, 'books/detail.html', {
     'book': book,
     'reader_form': reader_form
   })
+
 
 class BookCreate(CreateView):
   model = Book
@@ -41,3 +41,13 @@ class BookUpdate(UpdateView):
 class BookDelete(DeleteView):
   model = Book
   success_url = '/books/'
+
+def add_reader(request, book_id):
+  # create a ModelForm instance using the data in request.POST
+  form = ReaderForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    new_reader = form.save(commit=False)
+    new_reader.book_id = book_id
+    new_reader.save()
+  return redirect('detail', book_id=book_id)
